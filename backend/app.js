9bda203590b2a1addbdf22cc20d4a6fd7a5eac86
Kartik Marwal <kartikmarwal25@gmail.com>
2025-08-15 13:10:57 +0530

    trusted path

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://hlo-resume-rmn8-9kfykzrks-kartik-marwals-projects.vercel.app'] 
+    ? ['https://hlo-resume-rmn8.vercel.app/'] 
     : ['http://localhost:3000'],
   credentials: true
 }));
